bb59856cf31c945d317d75bcc02fd56608324048
grosser <grosser.michael@gmail.com>
2013-09-10 22:00:11 -0700

    proper light on/off

--- src/com/example/myfirstapp/MainActivity.java
+++ src/com/example/myfirstapp/MainActivity.java
@@ -1,14 +1,12 @@
 package com.example.myfirstapp;
 
-import android.os.Bundle;
 import android.app.Activity;
-import android.view.Menu;
-import android.graphics.Color;
+import android.content.pm.PackageManager;
 import android.hardware.Camera;
 import android.hardware.Camera.Parameters;
-import android.content.Context;
-import android.content.pm.PackageManager;
+import android.os.Bundle;
 import android.util.Log;
+import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
@@ -19,69 +17,70 @@ public class MainActivity extends Activity {
 	private Camera camera;
 	private Button button;
 	private View v;
-	
+
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-		
+
 		button = (Button) findViewById(R.id.buttonFlashlight);
-		camera = Camera.open();
-		final Parameters p = camera.getParameters();
-		
 		button.setOnClickListener(new OnClickListener() {
 			@Override
 			public void onClick(View arg0) {
-
 				if (isLightOn) {
-					Log.e("err", "Light off!");
-					isLightOn = false;
-
-					/*
-					 * INSERT CODE here that does the following:
-					 * 1. Prints to the LogCat the state you are about to change the light to ("Flashlight is on/off!")
-					 * 2. Turns on or off the flash (google "setFlashMode" on Parameters of the camera ('p')
-					 * 3. Starts or stops the preview (required for camera access) [e.g. camera.stopPreview(); is run when you want to switch it off]
-					 * 4. Changes the background color of our backgroundView (which we'ved named 'v' above)
-					 * 5. Adjusts the boolean value accordingly (so we can toggle it again next time)
-					 */
+					turnLightOff();
 				} else {
-					Log.e("err", "Light on!");
-					isLightOn = true;
-
-					/*
-					 * INSERT CODE here that does the following:
-					 * 1. Prints to the LogCat the state you are about to change the light to ("Flashlight is on/off!")
-					 * 2. Turns on or off the flash (google "setFlashMode" on Parameters of the camera ('p')
-					 * 3. Starts or stops the preview (required for camera access) [e.g. camera.stopPreview(); is run when you want to switch it off]
-					 * 4. Changes the background color of our backgroundView to Color.WHITE when the light is on and Color.BLACK when the light is off (which we'ved named 'v' above)
-					 * 5. Adjusts the boolean value accordingly (so we can toggle it again next time)
-					 */
+					turnLightOn();
 				}
 			}
 		});
-
 	}
 
 	@Override
+	// release camera so other apps can use it
 	protected void onStop() {
 		super.onStop();
+		turnLightOff();
+	}
 
-		/*
-		 * Here we handle the case when the app is Stopped (via exit or by the OS)
-		 * [no need for code here, just understand why we've put this here in the onStop Method of the application lifecycle]
-		 */
+	private void turnLightOff() {
+		Log.e("err", "Light off!");
+		isLightOn = false;
+
+		// TODO change color to black
+
 		if (camera != null) {
+			camera.stopPreview();
 			camera.release();
+			camera = null;
 		}
 	}
-	
-	
+
+	private void turnLightOn() {
+		Log.e("err", "Light on!");
+		isLightOn = true;
+
+		// TODO change color to white
+
+		if (hasFlash()) {
+			camera = Camera.open();
+			Parameters p = camera.getParameters();
+			p.setFlashMode(Parameters.FLASH_MODE_TORCH);
+			camera.setParameters(p);
+			camera.startPreview();
+		}
+	}
+
+	private boolean hasFlash(){
+		return getPackageManager().hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+	}
+
+
 	@Override
+	// TODO delete !?
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
-	
 }
